6dc07392ca7eb93062465490a3a262e80475cb1e
Jung217 <alex24922665@gmail.com>
2024-05-16 14:51:35 +0800

    Add Model chooser

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ line_bot_api = LineBotApi('lSYbbaNCUPI3GcZEak+tnX/4uEJCF/KjOkqXj5DYVQasBdf5NBixX
 handler = WebhookHandler('9b1a4bc1445da064bf4dd503a44944b7')
 
 line_bot_api.push_message('U9331f84776672cb357b3b8b9f89ebeaf', TextSendMessage(text='You can start !'))
-
+mmodel = "llama3-8b-8192"
 @app.route("/callback", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
@@ -34,38 +34,39 @@ def callback():
 @handler.add(MessageEvent)
 def handle_message(event):
     message = event.message.text
-    sendString = ""
 
     if re.match("提示",message):
-        remessage = "觸發驚喜的密語:\n\n恭喜\n今天我生日\n金門大學在哪\n\n試著輸入看看吧!"
+        remessage = "請自由發問，預設使用繁體中文回答。\n輸入'模型'可以更換模型\n\n如果我開始瘋狂說英文\n請對我說'Speak Chinese'\n\nThank you :)"
         line_bot_api.reply_message(event.reply_token,TextSendMessage(remessage))        
     
-    elif re.match("運勢",message):
+    elif re.match("模型",message):
         buttons_template_message = TemplateSendMessage(
         alt_text = "運勢",
         template=CarouselTemplate( 
             columns=[ 
                 CarouselColumn( 
                     thumbnail_image_url ="https://mednote.files.wordpress.com/2019/10/img_1689.jpg",
-                    title = "讓貓貓企鵝小助手\n為你測試運氣吧!", 
-                    text ="請選擇一種方法測", 
+                    title = "更換模型", 
+                    text ="請選擇一種 Model", 
                     actions =[
                         MessageAction( 
-                            label="星座",
-                            text="星座運勢")
+                            label="Gemma 7b",
+                            text="gemma-7b-it"),
+                        MessageAction( 
+                            label="LLaMA3 8b",
+                            text="llama3-8b-8192"),
+                        MessageAction( 
+                            label="LLaMA3 70b",
+                            text="llama3-70b-8192"),
+                        MessageAction( 
+                            label="Mixtral 8x7b",
+                            text="mixtral-8x7b-32768")
                         ]
                     )
                 ]
             )
         )
         line_bot_api.reply_message(event.reply_token, buttons_template_message)
-
-    elif re.match("更多功能",message):
-        image_message = ImageSendMessage(
-            original_content_url='https://raw.githubusercontent.com/Jung217/Penguin-cat-assistant/main/pic/tired.jpg',
-            preview_image_url='https://raw.githubusercontent.com/Jung217/Penguin-cat-assistant/main/pic/tired.jpg'
-        )
-        line_bot_api.reply_message(event.reply_token, image_message)
 
     else:
         client = Groq(
@@ -76,10 +77,10 @@ def handle_message(event):
             messages=[
                 {
                     "role": "user",
-                    "content": message + "，遊用繁體中文回答",
+                    "content": message + "，用繁體中文回答",
                 }
             ],
-            model="llama3-8b-8192",
+            model=mmodel,
         )  
         line_bot_api.reply_message(event.reply_token,TextSendMessage(chat_completion.choices[0].message.content))
 
